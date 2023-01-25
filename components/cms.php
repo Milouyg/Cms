381a1827b0332da9f76152f2e18e6479c487f2ea
Jacobwj04 <jacob@thewj5.com>
2023-01-25 09:27:50 +0100

    cms counter style

--- components/cms.php
+++ components/cms.php
@@ -66,7 +66,7 @@ move_uploaded_file($fileTempName, $fileBestemming);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
-    <script src="js/main.js" defer></script>
+    <script src="../js/main.js" defer></script>
     <title>CMS</title>
 </head>
 <body>
